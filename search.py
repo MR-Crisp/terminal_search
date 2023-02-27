ccc9285911df7b1c52964bd7991a1847ae045298
MR_Crisp <aka.amin2005@gmail.com>
2023-02-27 21:13:55 +0000

    finished all of the coding part of it just need to use doskey to make an alias and then it will work

--- search.py
+++ search.py
@@ -1,4 +1,21 @@
 import webbrowser
-url = 'http://docs.python.org/'
-chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
-webbrowser.get(chrome_path).open(url)
+import sys
+from pprint import pprint
+
+BASE_URL = 'https://www.google.com/search?q='
+CHROME_PATH = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+
+def create_query():
+    
+    query = sys.argv[1:]
+    return ' '.join(query) 
+    
+
+def create_url():
+    if len(sys.argv[1:]) == 0:
+        print('Please enter A valid argument')
+    else:
+        return BASE_URL+create_query()
+
+webbrowser.get(CHROME_PATH).open(create_url())
+ 
