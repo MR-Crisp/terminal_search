252684ad8d8b611fd138be2e9bef586790cf27b1
MR_Crisp <aka.amin2005@gmail.com>
2023-03-02 10:53:28 +0000

    Have now got it set up with windows CDM so i can now do an aliased terminal search using the `s` command. However have still yet to get it working on the IDE terminal and need to figure that out. As well as havent added my Filter settings yet

--- search.py
+++ search.py
@@ -8,7 +8,8 @@ CHROME_PATH = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
 def create_query():
     
     query = sys.argv[1:]
-    return ' '.join(query) 
+    
+    return str(' '.join(query)) 
     
 
 def create_url():
@@ -18,4 +19,8 @@ def create_url():
         return BASE_URL+create_query()
 
 webbrowser.get(CHROME_PATH).open(create_url())
+ 
+
+#Add DOSKEY to PATH variable: https://superuser.com/questions/988311/doskey-is-not-recognized-when-using-cmder
+ #For windows to set up an alias permenantly (I used the second method): https://superuser.com/questions/1134368/create-permanent-doskey-in-windows-cmd
  
